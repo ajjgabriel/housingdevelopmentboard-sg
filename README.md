65f4fbcf02cc8611ec8479d74944427b0ba8d32c
Gabriel <ajjgabriel@gmail.com>
2015-05-10 22:26:35 +0800

    Check For Intersection Readme to add build status

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 housingdevelopmentboard-sg
 ==========================
+<img src="https://drone.io/github.com/ajjgabriel/housingdevelopmentboard-sg/status.png">
+
 
 Housing Development Board Singapore
--- housingdevelopmentboard-sg.go
+++ housingdevelopmentboard-sg.go
@@ -3,6 +3,7 @@ package WhatsYourTimeNow
 import (
     "html/template"
     "net/http"
+	"math"
 )
 
 func init() {
@@ -12,10 +13,31 @@ func init() {
 
 func root(w http.ResponseWriter, r *http.Request) {
 	
-	timeEvent := "";
+	var arrayIntersect [2]int = interectCircle(1,1,1,1);
 	
+	whatsYourTimeNowForm.ExecuteTemplate(w, "housingdevelopmentboard-sg.htm", arrayIntersect);
+}
+
+func interectCircle(x1 float64, y1 float64,x2 float64, y2 float64) [2]int {
+	var x float64 = x1 - x2;
+	var y float64 = y1 - y2;
+	var distanceBetweenCenters float64 = math.Sqrt(math.Pow(2, x) + math.Pow(2,y));
 	
-	whatsYourTimeNowForm.ExecuteTemplate(w, "housingdevelopmentboard-sg.htm", timeEvent);
+	var sumOfRadiusOneKm float64 = 2;
+	var sumOfRadiusTwoKm float64 = 4;
+	var arrayIntersect [2]int
+	if(distanceBetweenCenters <= sumOfRadiusOneKm){
+	
+		arrayIntersect[0] = 1;
+	}
+	
+	if(distanceBetweenCenters <= sumOfRadiusTwoKm){
+	
+		arrayIntersect[1] = 1;
+	}
+	
+	return arrayIntersect;
+	
 }
 
 
